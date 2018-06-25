b4e3ffc1fcf21d79c0eb9bddbfcf3a8c8ed85132
Dewbud <jalfonso1134@gmail.com>
2018-06-25 18:04:26 -0400

    map properties
    map properties to key=>value instead of ['type','name','value']

--- src/Response.php
+++ src/Response.php
@@ -2,10 +2,33 @@
 
 namespace Dewbud\AgileCRM;
 
+use GuzzleHttp\Psr7\Response as GuzzleResponse;
+
 class Response extends Fluent
 {
-    public function __construct(array $res)
+    /** @var \GuzzleHttp\Psr7\Response */
+    public $guzzle_response;
+
+    public function __construct(GuzzleResponse $response)
     {
-        parent::__construct($res);
+        $this->guzzle_response = $response;
+
+        if ($response->getStatusCode() === 200) {
+            parent::__construct(\json_decode($response->getBody()));
+        } else {
+            parent::__construct();
+        }
+
+        if (array_key_exists('properties', $this->attributes)) {
+
+            // Map response properties to Fluent properties
+            $props = [];
+
+            foreach ($this->attributes['properties'] as $key => $field) {
+                $props[$field->name] = $field->value;
+            }
+
+            $this->attributes['properties'] = (object) $props;
+        }
     }
 }
